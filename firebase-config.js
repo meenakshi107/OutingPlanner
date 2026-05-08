4f8b26637ac833b466e883b0fd7e3e34ce539186
meenakshi107 <121838166+meenakshi107@users.noreply.github.com>
2026-05-08 12:03:24 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -6,13 +6,13 @@ import { initializeApp }       from "https://www.gstatic.com/firebasejs/10.12.0/
 import { getFirestore }      from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDllRhxbJQMP3GnTBnEemNhhP7sNM_0ZfA",
-  authDomain: "outingplanner-5d2fa.firebaseapp.com",
-  projectId: "outingplanner-5d2fa",
-  storageBucket: "outingplanner-5d2fa.firebasestorage.app",
-  messagingSenderId: "992263825988",
-  appId: "1:992263825988:web:4e2b4d58153a008a48ea11",
-  measurementId: "G-92CKZ0XKL1"
+  apiKey: "AIzaSyDljkZs3hWm6zQv-aYZp-BYNzgNNKYcfTM",
+  authDomain: "outingpoll.firebaseapp.com",
+  projectId: "outingpoll",
+  storageBucket: "outingpoll.firebasestorage.app",
+  messagingSenderId: "22100990738",
+  appId: "1:22100990738:web:8b3c0872fdcd17a733b1cb",
+  measurementId: "G-W6ENHGJN2F"
 };
 const app = initializeApp(firebaseConfig);
 const db  = getFirestore(app);
